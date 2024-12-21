4170ed6b02b8e3e268f053698cbd9d4cce5b3386
Josh Endemann <jahend44@gmail.com>
2024-12-21 09:54:08 -0600

    Implement mobile menu toggle with FaBars and FaTimes icons
    - Added state management for toggling the mobile menu.
    - Used FaBars and FaTimes from react-icons for mobile menu icon.
    - Added CSS class `active` to display the mobile menu when toggled.
    - Updated the Navbar component to handle mobile responsiveness.

--- down-to-earth/src/components/Navbar/Navbar.jsx
+++ down-to-earth/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import {FaBars} from 'react-icons/fa';
 
 
 export default function Navbar() {
+    const [menuOpen, setmenuOpen] = React.useState(false);
+    const toggleMenu = () => {
+        setmenuOpen(!menuOpen);
+    }
+
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to='/' className='navbar-logo'>
                     <img src='./cover.jpg' alt='logo' className='navbar-logo-img' />
                 </Link>
-                <ul className='navbar-menu'>
+                <div className='menu-icon' onClick={toggleMenu}>
+                        <i className={menuOpen ? 'fas fa-times' : 'fas fa-bars'}>
+                            <FaBars />
+                        </i>
+                    </div>
+                <ul className={menuOpen ? 'navbar-menu active' : 'navbar-menu'}>
                     <li className='navbar-item'>
                         <Link to='/' className='navbar-links'>Home</Link>
                     </li>
+                   
                     <li className='navbar-item'>
                         <Link to='/about' className='navbar-links'>About</Link>
                     </li>
