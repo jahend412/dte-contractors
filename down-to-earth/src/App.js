d6d160e6d0eac7b74036349d5dca6d23f3294651
Josh Endemann <jahend44@gmail.com>
2024-12-21 10:10:40 -0600

    Update App.js
    Add Home Component

--- down-to-earth/src/App.js
+++ down-to-earth/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from  './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Home />
     </BrowserRouter>
     
   );
