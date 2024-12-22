3619bc462b39f8bb22f2b7d5baababe9221fd0ad
Josh Endemann <jahend44@gmail.com>
2024-12-22 12:34:03 -0600

    Update App.js
    Import and add Footer to app

--- down-to-earth/src/App.js
+++ down-to-earth/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from  './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Home />
+      <Footer />
     </BrowserRouter>
     
   );
