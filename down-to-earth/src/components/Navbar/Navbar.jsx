b758244fc1da18858d51836a3b52984678d6315c
Josh Endemann <jahend44@gmail.com>
2024-12-22 14:06:18 -0600

    Update Navbar.jsx
    - Added services for Navbar Dropdown Menu.
    - Added a dropdown box to Services that is activated when you hover over it.
    - Fixed the cover image.  Removed the period from the img source due to it being in public folder.  Wasn't showing up on my service/{service} pages

--- down-to-earth/src/components/Navbar/Navbar.jsx
+++ down-to-earth/src/components/Navbar/Navbar.jsx
@@ -10,11 +10,21 @@ export default function Navbar() {
         setmenuOpen(!menuOpen);
     }
 
+    const services = [
+        { title: "Sewer Laterals", link: "/services/sewer" },
+        { title: "Water Laterals", link: "/services/water" },
+        { title: "Septic Systems", link: "/services/septic" },
+        { title: "Excavation", link: "/services/excavation" },
+        { title: "Grading", link: "/services/grading" },
+        { title: "Demolition", link: "/services/demolition" },
+      ];
+    
+
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to='/' className='navbar-logo'>
-                    <img src='./cover.jpg' alt='logo' className='navbar-logo-img' />
+                    <img src='/cover.jpg' alt='logo' className='navbar-logo-img' />
                 </Link>
                 <div className='menu-icon' onClick={toggleMenu}>
                         <i className={menuOpen ? 'fas fa-times' : 'fas fa-bars'}>
@@ -29,8 +39,15 @@ export default function Navbar() {
                     <li className='navbar-item'>
                         <Link to='/about' className='navbar-links'>About</Link>
                     </li>
-                    <li className='navbar-item'>
-                        <Link to='/services' className='navbar-links'>Services</Link>
+                    <li className="navbar-item services-dropdown">
+                        <Link to="/services" className="navbar-links">Services</Link>
+                            <ul className="services-dropdown-menu">
+                                {services.map((service, index) => (
+                                    <li key={index}>
+                                        <Link to={service.link}>{service.title}</Link>
+                                    </li>
+                                ))}
+                            </ul>
                     </li>
                     <li className='navbar-item'>
                         <Link to='/gallery' className='navbar-links'>Gallery</Link>
